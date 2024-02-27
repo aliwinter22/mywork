887594007573c9ec80623b5dd5bfbae131f8fce7
Alem Winter <161452611+aliwinter22@users.noreply.github.com>
2024-02-27 12:15:14 +0200

    Create maincode.cs

---
+++ maincode.cs
@@ -0,0 +1,33 @@
+
+        internal string DebugDisplayString
+        {
+            get
+            {
+                return string.Concat(
+                    "Center( ", this.Center.DebugDisplayString, " )  \r\n",
+                    "Radius( ", this.Radius.ToString(), " )"
+                    );
+            }
+        }
+
+     public ContainmentType Contains(BoundingBox box)
+        {
+
+            bool inside = true;
+            foreach (Vector3 corner in box.GetCorners())
+            {
+                if (this.Contains(corner) == ContainmentType.Disjoint)
+                {
+                    inside = false;
+                    break;
+                }
+            }
+
+            if (inside)
+                return ContainmentType.Contains;
+
+        private CurveContinuity _continuity;
+        private readonly float _position;
+        private float _tangentIn;
+        private float _tangentOut;
+        private float _value;
